a90f8e04fd361a6bc96e4eecd40ad94a5ae8e4b8
grynhas <patrick.d.s.reis@gmail.com>
2022-02-15 22:47:36 -0300

    primeiro componente

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,26 @@
+export function RepositoryList() {
+  return (
+    <section className="repository-list" >
+      <h1>Lista de repositórios</h1>
+
+      <ul>
+        <li>
+          <strong>unForm</strong>
+          <p>Forms in React</p>
+
+          <a href="">
+            Acessar repositorio
+          </a>
+        </li>
+        <li>
+          <strong>unForm</strong>
+          <p>Forms in React</p>
+
+          <a href="">
+            Acessar repositorio
+          </a>
+        </li>
+      </ul>
+    </section>
+  )
+}
